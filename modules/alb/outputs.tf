c996307dcc3331cd7d2aace48f7bb4b811d4b5c0
Steffen <Redacted>
2026-05-07 00:29:00 -0500

    Added outputs to alb/compute stack

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -2,3 +2,13 @@ output "target_group_arn" {
   description = "The ARN of the target group"
   value       = aws_lb_target_group.target_group.arn
 }
+
+output "alb_zone_id" {
+  description = "The ARN of the target group"
+  value       = aws_lb.load_balancer.zone_id
+}
+
+output "alb_dns_name" {
+  description = "The ARN of the target group"
+  value       = aws_lb.load_balancer.dns_name
+}
---
+++ stacks/compute/outputs.tf
@@ -0,0 +1,9 @@
+output "alb_zone_id" {
+  description = "The ARN of the target group"
+  value       = module.alb.alb_zone_id
+}
+
+output "alb_dns_name" {
+  description = "The ARN of the target group"
+  value       = module.alb.alb_dns_name
+}
